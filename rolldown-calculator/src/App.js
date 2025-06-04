913d30a9432dae11d26f06ae4f000e08e31ffccd
JeremyTruong <jeremytruong@hotmail.com>
2025-06-03 23:27:52 -0400

    changed BrowserRouter to HashRouter

--- rolldown-calculator/src/App.js
+++ rolldown-calculator/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import AboutPage from './components/AboutPage';
 import RolldownCalc from './components/RolldownCalc';
